48407d5e11cc6c79155fb55f0937d848907013df
Gi Ppeum, Lee <48782921+pppeum62@users.noreply.github.com>
2020-05-06 19:29:31 +0900

    문제 풀이 - if 조건문을 switch 조건문으로 변경

--- ilikeswitch/Program.cs
+++ ilikeswitch/Program.cs
@@ -15,18 +15,23 @@ namespace ilikeswitch
             Console.Write("학년을 입력하세요 >> ");
             int level = int.Parse(Console.ReadLine());
 
-            if (level == 1)
+            switch(level)
             {
-                Console.WriteLine("수강해야 하는 전공 학점 : 12학점");
-            } else if (level == 2)
-            {
-                Console.WriteLine("수강해야 하는 전공 학점 : 18학점");
-            } else if (level == 3)
-            {
-                Console.WriteLine("수강해야 하는 전공 학점 : 10학점");
-            } else if (level == 4)
-            {
-                Console.WriteLine("수강해야 하는 전공 학점 : 18학점");
+                case 1:
+                    Console.WriteLine("수강해야 하는 전공 학점 : 12학점");
+                    break;
+                case 2:
+                    Console.WriteLine("수강해야 하는 전공 학점 : 18학점");
+                    break;
+                case 3:
+                    Console.WriteLine("수강해야 하는 전공 학점 : 10학점");
+                    break;
+                case 4:
+                    Console.WriteLine("수강해야 하는 전공 학점 : 18학점");
+                    break;
+                default:
+                    Console.WriteLine("입력 값을 확인해주세요");
+                    break;
             }
         }
     }
